ccce0e4d08ea3ce7541edc1ab3cb3f9e4ea44a59
JuanFe0209 <piperbueno@hotmail.com>
2023-09-14 12:33:47 -0500

    .

--- .idea/Usuarios.php
+++ .idea/Usuarios.php
@@ -3,9 +3,10 @@
 require_once "conexion.php";
 
 $nombre = filter_input(INPUT_POST,'nombre');
+$correo = filter_input(INPUT_POST, 'correo');
 $password = filter_input(INPUT_POST,'password');
 
-$sqlt = "INSERT INTO usuario (nombre,password)
+$sqlt = "INSERT INTO usuario (nombre,correo,password)
         VALUES ('$nombre','$password');";
 
 $sqlt = Conexion::conectar()->prepare($sqlt);
